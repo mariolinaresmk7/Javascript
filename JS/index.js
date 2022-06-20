1b110b69f84633765e93e8fdcdc46455d372c0a5
Mario Linares <exequiellinares306@gmail.com>
2022-06-20 20:52:51 -0300

    primerapreentrega

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-//Declaracion de variables
+// DECLARACION DE VARIABLES DENTRO DE UN OBJETO
 const banco = {
     totalAPagar: " ",
     montoDelCredito: " ",
@@ -7,129 +7,110 @@ const banco = {
     intereses: " ",
     coutas: " ",
 }
-//Declaracion de Funciones
-const tipodeintereses = (porcentaje) => {
-    cantidadDeCoutas = +prompt('Ingrese la cantidad de coutas que desea financiar su prestamo: 12, 24, 36 o 48 coutas.');
-    if (cantidadDeCoutas == 12) {
-        interesAnual = porcentaje
-    } else if (cantidadDeCoutas == 24) {
-        interesAnual = porcentaje * 2
-    } else if (cantidadDeCoutas == 36) {
-        interesAnual = porcentaje * 3
-    } else if (cantidadDeCoutas == 48) {
-        interesAnual = porcentaje * 4
-    } else {
-        alert('ingrese la cantidad de coutas estipuladas por la entidad bancario')
-        tipodeintereses(porcentaje)
-    }
-};
-const creditoPersonaloPrendario = (porcentaje) => {
-    banco.montoDelCredito = +prompt('Ingrese el monto a solicitar');
-    console.log('monto del credito a solicitar ' + banco.montoDelCredito);
-    tipodeintereses(porcentaje)
-    console.log('detalle de cantidad de coutas ' + cantidadDeCoutas + ' y portecentaje total de intereses ' + interesAnual)
-    if (((cantidadDeCoutas == 12) || (cantidadDeCoutas == 24)) || ((cantidadDeCoutas == 36) || (cantidadDeCoutas == 48))) {
-        intereses = interesAnual * banco.montoDelCredito;
-        console.log('monto de intereses ' + intereses)
-        banco.totalAPagar = intereses + banco.montoDelCredito;
-        console.log('total a pagar con intereses ' + banco.totalAPagar)
-        coutas = banco.totalAPagar / cantidadDeCoutas;
-        console.log('monto por coutas ' + coutas)
-        alert('Su total a pagar es de ' + cantidadDeCoutas + ' coutas de $' + coutas);
-    } else {
-        alert("Ingrese la cantidad de coutas estimadas por la entidad");
-        tipodeintereses(porcentaje);
-        alert('Error')
-    }
-};
-
-// Clase constructora
+// CLASS CONTRUCTORA
 class Usuario {
     constructor(nombre, apellido, usuarioBanco, dni, ) {
         this.nombre = nombre
         this.apellido = apellido
         this.usuarioBanco = usuarioBanco
         this.dni = dni
-
-    }
-    bonificacion1(prestamoP) {
-        return prestamoP - 0.20;
-    }
-    bonificacion2(prestamoP) {
-        return prestamoP - 0.10;
     }
 }
-//En funcionamiento
-usuarioBanco = prompt('Usted es cliente del banco: Si o No');
-console.log('Usuario de banco: ' + usuarioBanco.toUpperCase());
-if (usuarioBanco == 'si') {
-    nombre = prompt('ingrese su nombre');
-    apellido = prompt('ingrese su apellido');
-    dni = +prompt('ingrese su dni');
-    const cliente = new Usuario(nombre, apellido, usuarioBanco, dni, 0.65);
-    alert('Bienvenido ' + cliente.nombre.toUpperCase() + ' ' + cliente.apellido.toUpperCase())
-    alert('Por ser cliente le vamos a hacer una bonificacion en la tasa de interes del prestamo a solicitar')
-    let tipoDeCredito = prompt('Que tipo de credito desea solicitar: prendario o personal.');
-    console.log(tipoDeCredito)
-    if (tipoDeCredito == 'personal') {
-        let descuento = +cliente.bonificacion1(0.65);
-        creditoPersonaloPrendario(descuento)
-    } else if (tipoDeCredito == 'prendario') {
-        const cliente = new Usuario(nombre, apellido, usuarioBanco, dni, 0.35)
-        let descuento = +cliente.bonificacion2(0.35)
-        creditoPersonaloPrendario(descuento)
-    } else {
-        alert('Error, por favor ingrese el tipo de credito que desea');
-        for (i = 1; i <= 2; i++) {
-            tipoDeCredito = prompt('Prendario o Personal')
-            if (tipoDeCredito == 'personal') {
-                const cliente = new Usuario(nombre, apellido, usuarioBanco, dni, 0.35)
-                let descuento = +cliente.bonificacion2(0.35)
-                creditoPersonaloPrendario(descuento);
+
+//ARRAY DE COUTAS 
+const coutasPrestamoPrendario = [0.35, 0.70, 1.05, 1.4];
+const coutasPrestamoPersonal = [0.65, 1.3, 1.95, 2.6];
+console.log(coutasPrestamoPrendario);
+console.log(coutasPrestamoPersonal);
+
+// METODOS DE BUSQUEDA Y TRANSFORMACION
+const coutasPrestamoPersonalconBeneficios = coutasPrestamoPersonal.map(elem => elem - 0.20)
+const coutasPrestamoPrendarioconBeneficios = coutasPrestamoPrendario.map(elem => elem - 0.10)
+console.log(coutasPrestamoPrendarioconBeneficios)
+console.log(coutasPrestamoPersonalconBeneficios)
+
+// DECLARACION DE fUNCION DE ORDEN SUPERIOR
+function prestamosClientes() {
+    return function (array) {
+        banco.montoDelCredito = +prompt('Ingrese el monto a solicitar');
+        console.log('monto del credito a solicitar ' + banco.montoDelCredito);
+        cantidadDeCoutas = +prompt('Ingrese la cantidad de coutas que desea financiar su prestamo: 12, 24, 36 o 48 coutas.');
+        console.log('Cantidad de coutas ' + cantidadDeCoutas)
+        switch (cantidadDeCoutas) {
+            case 12:
+                intereses = array[0] * banco.montoDelCredito;
+                console.log('total de intereses: '+intereses)
                 break;
-            } else if (tipoDeCredito == 'prendario') {
-                const cliente = new Usuario(nombre, apellido, usuarioBanco, dni, 0.35)
-                let descuento = +cliente.bonificacion2(0.35)
-                creditoPersonaloPrendario(descuento)
+            case 24:
+                intereses = array[1] * banco.montoDelCredito;
+                console.log('total de intereses: '+intereses)
                 break;
-            } else {
-                alert('Error')
-            }
+            case 36:
+                intereses = array[2] * banco.montoDelCredito;
+                console.log('total de intereses: '+intereses)
+                break;
+            case 48:
+                intereses = array[3] * banco.montoDelCredito;
+                console.log('total de intereses: '+intereses)
+                break;
+            default:
+                alert('Vuelva a comenzar y coloque la cantidad de coutas estipuladas por el banco')
+                break;
         }
-    }
-} else {
-    alert('Simulacion de tu prestamo sin ser cliente del banco, para acceder a bonificaciones hacete nuestro cliente!!!')
-    let tipoDeCredito = prompt('Que tipo de credito desea solicitar: prendario o personal.');
-    console.log(tipoDeCredito)
-    if (tipoDeCredito == 'personal') {
-        creditoPersonaloPrendario(0.65);
-    } else if (tipoDeCredito == 'prendario') {
-        creditoPersonaloPrendario(0.35);
-    } else {
-        alert('Error, por favor ingrese el tipo de credito que desea');
-        for (i = 1; i <= 2; i++) {
-            tipoDeCredito = prompt('Prendario o Personal')
-            if (tipoDeCredito == 'personal') {
-                creditoPersonaloPrendario(0.65);
-                break;
-            } else if (tipoDeCredito == 'prendario') {
-                creditoPersonaloPrendario(0.35)
-                break;
-            } else {
-                alert('Error')
-            }
-        }
+        banco.totalAPagar = intereses + banco.montoDelCredito;
+        console.log('total a pagar con intereses ' + banco.totalAPagar)
+        coutas = banco.totalAPagar / cantidadDeCoutas;
+        console.log('monto por coutas ' + coutas)
+        alert('Su total a pagar es de ' + cantidadDeCoutas + ' coutas de $' + coutas);
     };
 }
 
-// Array Construct
-const listaCliente = [];
-listaCliente.push(new Usuario(nombre, apellido, usuarioBanco, dni));
-console.log(listaCliente.length);
-console.log(listaCliente)
-for (const usuario of listaCliente) {
-    console.log('Nombre: ' + usuario.nombre.toUpperCase());
-    console.log('Apellido: ' + usuario.apellido.toUpperCase());
-    console.log('Cliente del banco: ' + usuario.usuarioBanco.toUpperCase());
-    console.log('Dni: ' + usuario.dni);
+// EN FUNCIONAMIENTO
+usuarioBanco = prompt('Usted es cliente del banco: Si o No');
+console.log('Usuario de banco: ' + usuarioBanco.toUpperCase());
+if (usuarioBanco.toLowerCase() == 'si') {
+    nombre = prompt('ingrese su nombre');
+    apellido = prompt('ingrese su apellido');
+    dni = +prompt('ingrese su dni');
+    const cliente = new Usuario(nombre, apellido, usuarioBanco, dni);
+    alert('Bienvenido ' + cliente.nombre.toUpperCase() + ' ' + cliente.apellido.toUpperCase())
+    alert('Por ser cliente le vamos a hacer una bonificacion en la tasa de interes del prestamo a solicitar')
+    tipoDeCredito = prompt('Ingrese el tipo de prestamo que desea solicitar: prendario o personal')
+    console.log(tipoDeCredito);
+    if (tipoDeCredito == 'personal') { // if (tipoDeCredito == 'personal' || tipoDeCredito == '1')
+        let creiditosclientes = prestamosClientes();
+        console.log(creiditosclientes(coutasPrestamoPersonalconBeneficios))
+    } else if (tipoDeCredito == 'prendario') {
+        let creiditosclientes = prestamosClientes();
+        console.log(creiditosclientes(coutasPrestamoPrendarioconBeneficios))
+    }
+    // ARRAY DE CLIENTE 
+    const listaCliente = [];
+    listaCliente.push(new Usuario(nombre, apellido, usuarioBanco, dni));
+    console.log(listaCliente.length);
+    console.log(listaCliente)
+    for (const usuario of listaCliente) {
+        console.log('Nombre: ' + usuario.nombre.toUpperCase());
+        console.log('Apellido: ' + usuario.apellido.toUpperCase());
+        console.log('Cliente del banco: ' + usuario.usuarioBanco.toUpperCase());
+        console.log('Dni: ' + usuario.dni);
+    }
+} else if (usuarioBanco.toLowerCase() === 'no') {
+    alert('Simula tu prestamo')
+    let tipoDeCredito = prompt('Que tipo de credito desea solicitar: prendario o personal.');
+    console.log(tipoDeCredito)
+    if (tipoDeCredito == 'personal') {
+        const creditosNoCliente = prestamosClientes()
+        console.log(creditosNoCliente(coutasPrestamoPersonal))
+    } else if (tipoDeCredito == 'prendario') {
+        const creditosNoCliente = prestamosClientes()
+        console.log(creditosNoCliente(coutasPrestamoPrendario))
+    } else {
+        alert('Error, por favor ingrese el tipo de credito que desea');
+    };
+} else {
+    alert('Error, vuelva a comenzar y complete de forma correcta los datos')
 }
+
+
+
