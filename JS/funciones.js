2deec58915485ade445a7b1cb647e2a0804caeb8
Mario Linares <exequiellinares306@gmail.com>
2022-07-11 19:58:20 -0300

    librerias

--- JS/funciones.js
+++ JS/funciones.js
@@ -4,44 +4,37 @@ function prestamosClientes() {
     return function (array) {
         montoDelCredito = +document.getElementById('imonto').value;
         console.log('monto del credito a solicitar ' + montoDelCredito);
-        if (montoDelCredito < '1000') {
-            swal({
-                title: 'Simulador de credito',
-                text: 'Por favor, ingrese un monto superior a 1000',
-                icon: 'error',
-            });
-        } else {
-            cantidadDeCoutas = document.getElementById('cantidadDeCoutas');
-            if (cantidadDeCoutas.value === '0') {
-                swal({
-                    title: 'Simulador de credito',
-                    text: 'Por favor, seleccione la cantidad de coutas',
-                    icon: 'error',
-                })
-                return prestamosClientes();
-            } else if (cantidadDeCoutas.value === '12') {
-                intereses = array[0] * montoDelCredito;
-                console.log('total de intereses: ' + intereses)
-            } else if (cantidadDeCoutas.value === '24') {
-                intereses = array[1] * montoDelCredito;
-                console.log('total de intereses: ' + intereses)
-            } else if (cantidadDeCoutas.value === '36') {
-                intereses = array[2] * montoDelCredito;
-                console.log('total de intereses: ' + intereses)
-            } else if (cantidadDeCoutas.value === '48') {
-                intereses = array[3] * montoDelCredito;
-                console.log('total de intereses: ' + intereses)
-            }
-            totalAPagar = intereses + montoDelCredito;
-            console.log('total a pagar con intereses ' + totalAPagar)
-            coutas = totalAPagar / cantidadDeCoutas.value;
-            console.log('monto por coutas ' + coutas)
-            let total = document.getElementById('totalAPagar')
-            total.innerHTML = `<h3>Monto a solicitar es de $ ${montoDelCredito} </h3> 
-            <h3>Su total a pagar es de ${cantidadDeCoutas.value} coutas  de $ ${coutas}</h3>`
+        cantidadDeCoutas = document.getElementById('cantidadDeCoutas');
+        if (cantidadDeCoutas.value === '12') {
+            intereses = array[0] * montoDelCredito;
+            console.log('total de intereses: ' + intereses)
+        } else if (cantidadDeCoutas.value === '24') {
+            intereses = array[1] * montoDelCredito;
+            console.log('total de intereses: ' + intereses)
+        } else if (cantidadDeCoutas.value === '36') {
+            intereses = array[2] * montoDelCredito;
+            console.log('total de intereses: ' + intereses)
+        } else if (cantidadDeCoutas.value === '48') {
+            intereses = array[3] * montoDelCredito;
+            console.log('total de intereses: ' + intereses)
         }
-    };
-}
+        totalAPagar = intereses + montoDelCredito;
+        console.log('total a pagar con intereses ' + totalAPagar)
+        coutas = totalAPagar / cantidadDeCoutas.value;
+        console.log('monto por coutas ' + coutas)
+        tipoDeCredito = document.getElementById('tipoDePrestamo').value
+        swal({
+            title: "Simulador de Prestamo",
+            text: `
+                Bienvenido
+                Tipo de credito: ${tipoDeCredito}
+                Monto a solicitar es de $ ${montoDelCredito}
+                Su total a pagar es de ${cantidadDeCoutas.value} coutas  de $ ${coutas}`,
+            icon: "success"
+        })
+    }
+};
+
 
 // Las funciones storageUsuario y formularioStorageAndNotUser corresponde al localStorage que se le hizo al usuario
 function storageUsuario() {
@@ -55,19 +48,92 @@ function procesoStorage() {
     const activeLastName = localStorage.getItem('apellido');
     let storage = document.getElementById('storage');
     storage.innerHTML = `<h2>Retomemos donde dejaste</h2>
-                              <h5> ${activeName} ${activeLastName}</h5> `;
+                        <h5> ${activeName.toUpperCase()} ${activeLastName.toUpperCase()}</h5> `;
     formularioStorageAndNotUser();
-    resultado();
+    montoDelCredito = document.getElementById('imonto');
+    montoDelCredito.onblur = () => {
+        (montoDelCredito.value < 1000) && Toastify({
+            text: "ingrese un monto superior a 1000",
+            duration: 4000,
+            gravity: "top",
+            position: "center",
+            style: {
+                background: "#f85032;  /* fallback for old browsers",
+                background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                background: "linear-gradient(to right, #e73827, #f85032)"
+            }
+        }).showToast()
+    }
+
+    tipoDeCredito = document.getElementById('tipoDePrestamo');
+    tipoDeCredito.onchange = () => {
+        if (tipoDeCredito.value === '0') {
+            Toastify({
+                text: "Por favor seleccione el tipo de prestamo",
+                duration: 4000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#f85032;  /* fallback for old browsers",
+                    background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                    background: "linear-gradient(to right, #e73827, #f85032)"
+                }
+            }).showToast()
+        } else {
+            Toastify({
+                text: "tipo de prestamo seleccionado: " + tipoDePrestamo.value.toUpperCase(),
+                duration: 4000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#f85032;  /* fallback for old browsers",
+                    background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                    background: "linear-gradient(to right, #e73827, #f85032)"
+                }
+            }).showToast()
+        }
+    }
+
+    cantidadDeCoutas = document.getElementById('cantidadDeCoutas');
+    cantidadDeCoutas.onchange = () => {
+        if (cantidadDeCoutas.value === "0") {
+            Toastify({
+                text: "Por favor seleccione la cantidad de coutas",
+                duration: 4000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#f85032;  /* fallback for old browsers",
+                    background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                    background: "linear-gradient(to right, #e73827, #f85032)"
+                }
+            }).showToast()
+        } else {
+            Toastify({
+                text: 'selecciono' + ' ' + cantidadDeCoutas.value + ' ' + 'coutas',
+                duration: 4000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#f85032;  /* fallback for old browsers",
+                    background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                    background: "linear-gradient(to right, #e73827, #f85032)"
+                }
+            }).showToast()
+        }
+    }
     let simularPrestamo3 = document.getElementById('simularPrestamo3');
     simularPrestamo3.onclick = (e) => {
         e.preventDefault();
         let tipoDeCredito = document.getElementById('tipoDePrestamo');
+        montoDelCredito = document.getElementById('imonto');
+        cantidadDeCoutas = document.getElementById('cantidadDeCoutas');
         console.log(`Tipo de credito: ${tipoDeCredito.value}`);
-        if (tipoDeCredito.value == '0') {
+        if (montoDelCredito.value < 1000 || cantidadDeCoutas.value === "0" || tipoDeCredito.value === '0') {
             swal({
-                title: 'Simulador de credito',
-                text: 'Por favor, seleccione el tipo de credito a solicitar',
-                icon: 'error',
+                width: "800",
+                text: "Por favor revise los datos ingresados",
+                icon: "error"
             })
         } else if (tipoDeCredito.value == 'prendario') {
             let creiditosclientes = prestamosClientes();
@@ -76,107 +142,69 @@ function procesoStorage() {
             let creiditosclientes = prestamosClientes();
             console.log(creiditosclientes(coutasPrestamoPersonalconBeneficios))
         }
-        let tipoDePrestamo_ = document.getElementById('tipoDeCredito')
-        tipoDePrestamo_.innerHTML = `<h2>Tipo de prestamo: ${tipoDeCredito.value}</h2>`
-    }
-    let reload = document.getElementById('reload');
-    reload.onclick = () => {
-        location.reload();
     }
 }
 
 
+
+
 function formularioStorageAndNotUser() { //Funcion para formulario de storage y no usuarios
-    let formularioPadre = document.getElementById('formularioPadre')
-    formularioPadre.innerHTML = `<div class="col-lg-4 offset-4 tipoDePrestamo">
+    let formularioPadre = document.getElementById('user')
+    formularioPadre.innerHTML = `
+ <div>
     <!-- TIPO DE CREDITO A SOLICITAR -->
-    <label for="tipoDePrestamo">¿Que tipo de prestamos deseas solicitar?</label>
-    <select name="" id="tipoDePrestamo">
-        <option value="0">Seleccione una opción</option>
+    <select class="form-select mt-3 mb-3" aria-label="Default select example" id="tipoDePrestamo">
+        <option value="0">Tipo de prestamo a solicitar</option>
         <option value="prendario">Prendario</option>
         <option value="personal">Personal</option>
     </select>
-    </div>
-    <div class="col-lg-4 offset-4 contenedorHijo2">
-    <!-- MONTO DEL CREDITO A SOLICITAR -->
-    <label for="imonto">Ingrese el monto que desea solicitar</label>
-    <input type="number" name="monto" id="imonto" >
-    <!-- LA CANTIDAD DE COUTAS A FINANCIAR EL CREDITO -->
-    <label for="cantidadDeCoutas">Seleccione la cantidad de coutas</label>
-    <select name="" id="cantidadDeCoutas">
-        <option value="0">Seleccione una opción</option>
-        <option value="12">12 Coutas</option>
-        <option value="24">24 Coutas</option>
-        <option value="36">36 Coutas</option>
-        <option value="48">48 Coutas</option>
-    </select>
-    </div>
-    <div class="botones">
-    <input type="submit" value="Simular prestamo"  id="simularPrestamo3"  class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#myModal" >
+</div>
+<div><!-- MONTO DEL CREDITO A SOLICITAR -->
+    <input type="number" id="imonto" class="form-control" placeholder="Ingrese el monto"
+                aria-label="Default">
+</div>
+<div><!-- LA CANTIDAD DE COUTAS A FINANCIAR EL CREDITO -->
+    <select class="form-select mt-3" aria-label="Default select example" id="cantidadDeCoutas">
+        <option value="0" >Seleccione la cantidad de coutas</option>
+        <option value="12">12</option>
+        <option value="24">24</option>
+        <option value="36">36</option>
+        <option value="48">48</option>
+     </select> 
+</div>
+<input type="submit" value="Simular prestamo"  id="simularPrestamo3"  class="btn btn-secondary mt-3 " data-bs-toggle="modal" data-bs-target="#myModal" >
     `
 }
 
 function formularioParaClientes() { //Funcion para crear formulario para clientes 
-    let formularioPadre = document.getElementById('formularioPadre')
+    let formularioPadre = document.getElementById('user')
     formularioPadre.innerHTML = `
-    <div class="col-lg-4 datosCliente offset-4" id="datosCliente">
-    <!-- DATOS NOMBRE/APELLIDO/DNI -->
-    <label for="iname">Nombre</label>
-    <input type="text" name="name" id="iname">
-    <label for="ilastname">Apellido</label>
-    <input type="text" name="lastname" id="ilastname">
-    <label for="idni">DNI</label>
-    <input type="number" name="numberdni" id="idni">
-</div>
-    <div class="col-lg-4 offset-4 tipoDePrestamo">
+    <div id="datosCliente"><!-- DATOS NOMBRE/APELLIDO/DNI -->
+        <input type="text" id="iname" placeholder="Nombre" class="form-control mt-3 mb-3" aria-label="Default"  >
+        <input type="text" id="ilastname" placeholder="Apellido" class="form-control mb-3" aria-label="Default">
+        <input type="number"  id="idni" placeholder="DNI" class="form-control" aria-label="Default">
+    </div>
+    <div>
         <!-- TIPO DE CREDITO A SOLICITAR -->
-        <label for="tipoDePrestamo">¿Que tipo de prestamos deseas solicitar?</label>
-        <select name="" id="tipoDePrestamo">
-            <option value="0">Seleccione una opción</option>
+        <select class="form-select mt-3 mb-3" aria-label="Default select example" id="tipoDePrestamo">
+            <option value="0">Tipo de prestamo a solicitar</option>
             <option value="prendario">Prendario</option>
             <option value="personal">Personal</option>
         </select>
     </div>
-    <div class="col-lg-4 offset-4 contenedorHijo2">
-        <!-- MONTO DEL CREDITO A SOLICITAR -->
-        <label for="imonto">Ingrese el monto que desea solicitar</label>
-        <input type="number" name="monto" id="imonto" >
-        <!-- LA CANTIDAD DE COUTAS A FINANCIAR EL CREDITO -->
-        <label for="cantidadDeCoutas">Seleccione la cantidad de coutas</label>
-        <select name="" id="cantidadDeCoutas">
-            <option value="0">Seleccione una opción</option>
-            <option value="12">12 Coutas</option>
-            <option value="24">24 Coutas</option>
-            <option value="36">36 Coutas</option>
-            <option value="48">48 Coutas</option>
-        </select>
+    <div><!-- MONTO DEL CREDITO A SOLICITAR -->
+        <input type="number" id="imonto" class="form-control" placeholder="Ingrese el monto"
+                    aria-label="Default">
     </div>
-    <div class="botones">
-        <input type="submit" value="Simular prestamo"  id="simularPrestamo"  class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#myModal" >
+    <div><!-- LA CANTIDAD DE COUTAS A FINANCIAR EL CREDITO -->
+        <select class="form-select mt-3" aria-label="Default select example" id="cantidadDeCoutas">
+            <option value="0" >Seleccione la cantidad de coutas</option>
+            <option value="12">12</option>
+            <option value="24">24</option>
+            <option value="36">36</option>
+            <option value="48">48</option>
+         </select> 
+    </div>
+    <input type="submit" value="Simular prestamo"  id="simularPrestamo"  class="btn btn-secondary mt-3" data-bs-toggle="modal" data-bs-target="#myModal" >
       `
-}
-
-function resultado() {
-    let mensajeResultado = document.getElementById('mensajeResultado');
-    mensajeResultado.innerHTML = `  
-    <div class="modal fade" id="myModal" >
-        <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="modal-title tituloDeResultados" id="tituloDeResultados">Resultado de la simulación</h2>
-                </div>
-                <div class="modal-body usuarioResultado" id="mensajeBonificacion" >
-                    <p>Por ser nuestro cliente le vamos hacer una bonificacion en la tasa de interes del prestamos a solicitar</p>
-                </div>
-                <div class="modal-body usuarioResultado" id="mensaje"></div>
-                <div class="modal-body usuarioResultado" id="usuario" ></div>
-                <div class="modal-body usuarioResultado" id="tipoDeCredito" ></div>
-                <div class="modal-body usuarioResultado" id="totalAPagar"></div>
-                <div class="modal-footer">
-                    <button class="btn btn-danger" data-bs-dismiss="modal" id="reload">Cerrar</button>
-                </div>
-            </div>
-        </div>
-    </div> 
-</div> `
 }
--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,81 @@
 // EN FUNCIONAMIENTO
-botonSimulacion.onclick = (e) => {
+let user = document.getElementById('radio');
+user.onchange = (e) => {
     e.preventDefault();
     let usuarioBanco = ''
     if (usuarioBanco1.checked) {
         usuarioBanco = usuarioBanco1.value;
         formularioParaClientes();
-        resultado();
+        montoDelCredito = document.getElementById('imonto');
+        montoDelCredito.onblur = () => {
+            (montoDelCredito.value < 1000) && Toastify({
+                text: "ingrese un monto superior a 1000",
+                duration: 4000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#f85032;  /* fallback for old browsers",
+                    background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                    background: "linear-gradient(to right, #e73827, #f85032)"
+                }
+            }).showToast()
+        }
+        tipoDeCredito = document.getElementById('tipoDePrestamo');
+        tipoDeCredito.onblur = () => {
+            if (tipoDeCredito.value === '0') {
+                Toastify({
+                    text: "Por favor seleccione el tipo de prestamo",
+                    duration: 4000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        background: "#f85032;  /* fallback for old browsers",
+                        background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                        background: "linear-gradient(to right, #e73827, #f85032)"
+                    }
+                }).showToast()
+            } else {
+                Toastify({
+                    text: "tipo de prestamo seleccionado: " + tipoDePrestamo.value.toUpperCase(),
+                    duration: 4000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        background: "#f85032;  /* fallback for old browsers",
+                        background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                        background: "linear-gradient(to right, #e73827, #f85032)"
+                    }
+                }).showToast()
+            }
+        }
+        cantidadDeCoutas = document.getElementById('cantidadDeCoutas');
+        cantidadDeCoutas.onblur = () => {
+            if (cantidadDeCoutas.value === "0") {
+                Toastify({
+                    text: "Por favor seleccione la cantidad de coutas",
+                    duration: 4000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        background: "#f85032;  /* fallback for old browsers",
+                        background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                        background: "linear-gradient(to right, #e73827, #f85032)"
+                    }
+                }).showToast()
+            } else {
+                Toastify({
+                    text: 'selecciono' + ' ' + cantidadDeCoutas.value + ' ' + 'coutas',
+                    duration: 4000,
+                    gravity: "top",
+                    position: "center",
+                    style: {
+                        background: "#f85032;  /* fallback for old browsers",
+                        background: "-webkit-linear-gradient(to right, #e73827, #f85032)",
+                        background: "linear-gradient(to right, #e73827, #f85032)"
+                    }
+                }).showToast()
+            }
+        }
         let simularPrestamo = document.getElementById('simularPrestamo');
         simularPrestamo.onclick = (e) => {
             e.preventDefault();
@@ -17,58 +87,36 @@ botonSimulacion.onclick = (e) => {
             localStorage.setItem('nombre', nombre);
             localStorage.setItem('apellido', apellido)
             dni = document.getElementById('idni').value;
-            if (nombre.length < '3' || apellido.length < '3') {
-                let mensajeBonificacion1 = document.getElementById('mensajeBonificacion');
-                mensajeBonificacion1.remove();
-                let mensajeNombreyApellido = document.getElementById('mensaje');
-                mensajeNombreyApellido.innerHTML = `<h2>Por favor ingrese de forma correcta su nombre y apellido</h2>`;
-                simularPrestamo.onclick();
-                let tipoDePrestamo_ = document.getElementById('tipoDeCredito');
-                tipoDePrestamo_.remove();
-                nuevoUsuario.remove();
-            } else {
-                let tipoDeCredito = document.getElementById('tipoDePrestamo');
-                console.log(`Tipo de credito: ${tipoDeCredito.value}`);
-                let tipoDePrestamo_ = document.getElementById('tipoDeCredito')
-                tipoDePrestamo_.innerHTML = `<h2>Tipo de prestamo: ${tipoDeCredito.value}</h2>`
-                let nuevoUsuario = document.getElementById('usuario')
-                nuevoUsuario.innerHTML = `  <h2>Bienvenido</h2>
-                                            <h4>Nombre y Apellido: ${nombre.toUpperCase()}, ${apellido.toUpperCase()}.</h4>
-                                            <p>DNI: ${dni}</p>`
-                if (tipoDeCredito.value == '0') {
-                    swal({
-                        title: 'Simulador de credito',
-                        text: 'Por favor, seleccione el tipo de credito a solicitar',
-                        icon: 'error',
-                    })
-                } else if (tipoDeCredito.value == 'prendario') {
-                    let creiditosclientes = prestamosClientes();
-                    console.log(creiditosclientes(coutasPrestamoPrendarioconBeneficios))
-                } else if (tipoDeCredito.value == 'personal') {
-                    let creiditosclientes = prestamosClientes();
-                    console.log(creiditosclientes(coutasPrestamoPersonalconBeneficios))
-                }
-                const listaCliente = [];
-                listaCliente.push(new Usuario(nombre, apellido, usuarioBanco, dni));
-                console.log(listaCliente.length);
-                console.log(...listaCliente)
-                for (const usuario of listaCliente) {
-                    console.log('Nombre: ' + usuario.nombre.toUpperCase());
-                    console.log('Apellido: ' + usuario.apellido.toUpperCase());
-                    console.log('Cliente del banco: ' + usuario.usuarioBanco.toUpperCase());
-                    console.log('Dni: ' + usuario.dni);
-                }
-                const en_json = JSON.stringify(listaCliente);
-                localStorage.setItem('listaCliente', en_json);
-                const usuario1 = JSON.parse(en_json)
-                const usuario3 = JSON.parse(localStorage.getItem('listaCliente'));
+            tipoDeCredito = document.getElementById('tipoDePrestamo');
+            console.log(`Tipo de credito: ${tipoDeCredito.value}`);
+            if (nombre.length < '3' || apellido.length < '3' || montoDelCredito.value < 1000 || cantidadDeCoutas.value === "0" || tipoDeCredito.value === '0') {
+                swal({
+                    text: "Por favor revise los datos ingresados",
+                    icon: "error"
+                })
+            } else if (tipoDeCredito.value === 'prendario') {
+                let creiditosclientes = prestamosClientes();
+                console.log(creiditosclientes(coutasPrestamoPrendarioconBeneficios))
+            } else if (tipoDeCredito.value === 'personal') {
+                let creiditosclientes = prestamosClientes();
+                console.log(creiditosclientes(coutasPrestamoPersonalconBeneficios))
             }
+            const listaCliente = [];
+            listaCliente.push(new Usuario(nombre, apellido, usuarioBanco, dni));
+            console.log(listaCliente.length);
+            console.log(...listaCliente)
+            for (const usuario of listaCliente) {
+                console.log('Nombre: ' + usuario.nombre.toUpperCase());
+                console.log('Apellido: ' + usuario.apellido.toUpperCase());
+                console.log('Cliente del banco: ' + usuario.usuarioBanco.toUpperCase());
+                console.log('Dni: ' + usuario.dni);
+            }
+            const en_json = JSON.stringify(listaCliente);
+            localStorage.setItem('listaCliente', en_json);
+            const usuario1 = JSON.parse(en_json)
+            const usuario3 = JSON.parse(localStorage.getItem('listaCliente'));
         }
         storageUsuario();
-        let reload = document.getElementById('reload');
-        reload.onclick = () => {
-            location.reload();
-        }
     } else if (usuarioBanco2.checked) {
         usuarioBanco = usuarioBanco2.value
         console.log(`Usuario del banco: ${usuarioBanco.toUpperCase()}`);
@@ -76,31 +124,21 @@ botonSimulacion.onclick = (e) => {
         let simularPrestamo3 = document.getElementById('simularPrestamo3');
         simularPrestamo3.onclick = (e) => {
             e.preventDefault();
-            let mensajeBonificacion1 = document.getElementById('mensajeBonificacion');
-            mensajeBonificacion1.remove();
             let tipoDeCredito = document.getElementById('tipoDePrestamo')
             console.log(`Tipo de credito: ${tipoDeCredito.value}`);
-            let tipoDePrestamo_ = document.getElementById('tipoDeCredito')
-            tipoDePrestamo_.innerHTML = `<h2>Tipo de prestamo: ${tipoDeCredito.value}</h2>`
-            if (tipoDeCredito.value === '0') {
+            montoDelCredito = document.getElementById("imonto")
+            if (montoDelCredito.value < 1000 || cantidadDeCoutas.value === "0" || tipoDeCredito.value === '0') {
                 swal({
-                    title: 'Simulador de credito',
-                    text: 'Por favor, seleccione el tipo de credito a solicitar',
+                    text: 'Por favor, verifique los datos ingresados',
                     icon: 'error',
                 })
-            } else if (tipoDeCredito.value == 'prendario') {
+            } else if (tipoDeCredito.value === 'prendario') {
                 const creditosNoCliente = prestamosClientes()
                 console.log(creditosNoCliente(coutasPrestamoPrendario))
-            } else if (tipoDeCredito.value == 'personal') {
+            } else if (tipoDeCredito.value === 'personal') {
                 const creditosNoCliente = prestamosClientes()
                 console.log(creditosNoCliente(coutasPrestamoPersonal))
             }
         }
-        resultado();
-        let reload = document.getElementById('reload');
-        reload.onclick = () => {
-            location.reload();
-        }
     }
-
 }
